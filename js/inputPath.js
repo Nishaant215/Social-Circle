3ee3ca1392a4c78bfac1896d339c5c3178019a99
Paul Pfeister <33327613+paulpfeister@users.noreply.github.com>
2017-11-12 09:08:10 -0500

    Add files via upload

--- js/inputPath.js
+++ js/inputPath.js
@@ -2,6 +2,7 @@ function goForward() {
     var checkEl = document.getElementById("step1");
 	var updateEl = document.getElementById("step2");
 	var updateLeft = document.getElementById("left");
+	var updateRight= document.getElementById("right");
     if (checkEl.style.display === "inline") {
         checkEl.style.display = "none";
 		updateEl.style.display = "inline";
@@ -24,8 +25,10 @@ function goForward() {
     }
 	checkEl = document.getElementById("step4");
     if (checkEl.style.display === "inline") {
+        updateEl = document.getElementById("step5");
         checkEl.style.display = "none";
-		// TODO : Redirect to food pages and send form
+		updateEl.style.display = "inline";
+		updateRight.style.display = "none";
     	return;
     }
 }
@@ -34,6 +37,7 @@ function goBack() {
     var checkEl = document.getElementById("step2");
 	var updateEl = document.getElementById("step1");
 	var updateLeft = document.getElementById("left");
+	var updateRight= document.getElementById("right");
     if (checkEl.style.display === "inline") {
         checkEl.style.display = "none";
 		updateEl.style.display = "inline";
@@ -52,6 +56,14 @@ function goBack() {
 		updateEl = document.getElementById("step3");
         checkEl.style.display = "none";
 		updateEl.style.display = "inline";
+    	return;
+    }
+	checkEl = document.getElementById("step5");
+	if (checkEl.style.display === "inline") {
+		updateEl = document.getElementById("step4");
+        checkEl.style.display = "none";
+		updateEl.style.display = "inline";
+		updateRight.style.display = "inline";
     	return;
     }
 }
